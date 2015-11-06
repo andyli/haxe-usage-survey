f4846c80448bdbf75f8ed1955c857252cd998d99
Andy Li <andy@onthewings.net>
2015-11-07 01:11:09 +0800

    changed color for exp and use stacked bar for fig_create

--- src/Analyzer.hx
+++ src/Analyzer.hx
@@ -5,6 +5,7 @@ import textwrap.*;
 import matplotlib.pyplot.Pyplot as Plt;
 import seaborn.Seaborn as Sns;
 import Main;
+import PyHelpers.Slice.*;
 using PyHelpers;
 using Lambda;
 
@@ -17,11 +18,12 @@ class Analyzer {
 			return '$pc% ($val)';
 		}
 	}
+	static public var expKeys = [v_pro_main, v_pro_occ, v_use, v_interested];
+	static public var expColors = Sns.cubehelix_palette.call(expKeys.length, rot=>-.75, reverse=>true);
 	static public function analyzeExp(data:DataFrame):Void {
 		var group:pandas.core.groupby.GroupBy = data.groupby(k_exp);
 		var groups:Dict<String,Dynamic> = group.groups;
-		var groupKeys = [v_pro_main, v_pro_occ, v_use, v_interested];
-		var values = [for (k in groupKeys) len(groups.get(k))];
+		var values = [for (k in expKeys) len(groups.get(k))];
 		Plt.subplot();
 		Plt.axes.call(aspect=>1);
 		Plt.title.call(
@@ -30,14 +32,14 @@ class Analyzer {
 		);
 		Plt.pie.call(
 			values,
-			labels => [for (k in groupKeys) {
+			labels => [for (k in expKeys) {
 				var l = Main.values[k_exp][k][0];
 				(Textwrap.wrap(l, 25):Array<String>).join("\n");
 			}],
 			labeldistance => 1.2,
 			autopct => make_autopct(values),
 			pctdistance => 0.7,
-			colors => Sns.color_palette("muted", groupKeys.length),
+			colors => expColors, //Sns.color_palette("muted", expKeys.length),
 			textprops => Lib.anonAsDict({
 				backgroundcolor: [1.0,1.0,1.0,0.9],
 			}),
@@ -47,7 +49,7 @@ class Analyzer {
 			})
 		);
 		// Plt.legend.call(
-		// 	labels => [for (k in groupKeys) Main.values[k_exp][k][0]],
+		// 	labels => [for (k in expKeys) Main.values[k_exp][k][0]],
 		// 	loc => "best"
 		// );
 		Plt.tight_layout();
@@ -57,35 +59,63 @@ class Analyzer {
 
 	static public function analyzeCreate(data:DataFrame):Void {
 		var vnames = [v_game, v_web_front, v_web_back, v_app_desktop, v_app_mobile, v_lib, v_hardware, v_art, v_not_sure, v_others];
-		var values = [
-			for (vname in vnames)
-			data.get(k_create + "_" + vname).sum()
-		];
 		var df = new DataFrame(Lib.anonAsDict({
 			"what": [for (n in vnames) 
 				if (n == v_others)
 					"Others"
 				else
 					Main.values[k_create][n][0]
-			], 
-			"count": values,
+			]
 		}));
+		for (exp in expKeys) {
+			df.__setitem__(exp, [
+				for (vname in vnames)
+				data.get(data.get(k_exp) == exp).get(k_create + "_" + vname).sum()
+			]);
+		}
+		df.__setitem__("total", [
+			for (vname in vnames)
+			data.get(k_create + "_" + vname).sum()
+		]);
 		trace(df);
 		// data.get(k_create)
 		// var df = new 
 		var ax:matplotlib.axes.Axes = Plt.subplot();
-		Sns.barplot.call(
-			x => "count",
-			y => "what",
-			data => df,
-			color => "b"
-		);
+		for (i in 0...expKeys.length) {
+			var i = expKeys.length - i - 1;
+			Sns.barplot.call(
+				x => [
+					for (_i in 0...i+1)
+					df.get(expKeys[_i])
+				].fold(function(a,b) return a + b, 0),
+				y => "what",
+				data => df,
+				color => expColors[i],
+				linewidth => 0,
+				label => Main.values[k_exp][expKeys[i]][0]
+			);
+		}
 		ax.set.call(
-			title => Main.colQuestions[k_create],
 			ylabel => "",
-			xlabel => "count",
+			xlabel => "Number of people",
 			xlim => [0, len(data.index)]
 		);
+		ax.set_title.call(
+			Main.colQuestions[k_create],
+			fontsize => "large"
+		);
+		var hl:Tuple<Dynamic> = ax.get_legend_handles_labels();
+		var legend = ax.legend.call(
+			untyped reversed(hl[0]),
+			untyped reversed(hl[1]),
+			loc => "lower right",
+			frameon => true,
+			framealpha => 0.8
+		);
+		var rect:matplotlib.patches.Rectangle = legend.get_frame();
+		rect.set_linewidth(1);
+		rect.set_edgecolor([1,1,1]);
+		rect.set_facecolor([1,1,1]);
 		Plt.tight_layout();
 		Plt.savefig.call("out/fig_create.png");
 		Plt.savefig.call("out/fig_create.svg");
